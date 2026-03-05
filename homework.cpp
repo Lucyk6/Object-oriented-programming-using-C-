c9c0a28d0ca95cc80fb67f5596a38bae4e54c9cd
Lucyk6 <lucykazaryan16@gmail.com>
2026-03-05 19:11:15 +0900

    Create homework.cpp

---
+++ homework.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+//class Book {
+//
+//public: 
+//	string title;
+//	string author;
+//	int pages;
+// 
+//	void displayInfo() {
+//
+//		cout << "title: "  << title << " The author: " << author << " Page: " << pages << endl;
+//	}
+//};
+//int main() {
+//
+//	Book b1;
+//	b1.title = "Kolobok";
+//	b1.author = "narod";
+//	b1.pages = 20;
+//	b1.displayInfo();
+//	return 0;
+//}
+//2
+//class Student {
+//
+//private:
+//	string name;
+//	int age;
+//
+//public: 
+//     Student(string n, int a)
+//	{
+//     
+//		name = n;
+//		age = a;
+//
+//	 }
+//    
+//	void display() {
+//
+//		cout << "Name: " << name << " Age: " << age << endl;
+//
+//	}
+//
+//};
+//int main() {
+//
+//	Student s1("Luisne", 18);
+//	s1.display();
+//	return 0;
+//
+//}
+//4
+//class Car
+//{
+//private:
+//    int speed;
+//
+//public:
+//    void setSpeed(int s)
+//    {
+//        if (s >= 0)
+//            speed = s;
+//        else
+//            cout << "Speed cannot be negative!" << endl;
+//    }
+//
+//    int getSpeed()
+//    {
+//        return speed;
+//    }
+//};
+//
+//
+//int main()
+//{
+//    Car car1;
+//    car1.setSpeed(100);
+//    cout << "Car speed: " << car1.getSpeed() << endl;
+//
+//    car1.setSpeed(-10);
+//
+//
+//    return 0;
+//}
+//5
+//#include <string>
+//class Person
+//{
+//protected:
+//    string name;
+//    int age;
+//
+//public:
+//    Person(string n, int a)
+//    {
+//        name = n;
+//        age = a;
+//    }
+//
+//    void displayPerson()
+//    {
+//        cout << "Name: " << name << ", Age: " << age << endl;
+//    }
+//};
+//
+//class Employee : public Person
+//{
+//private:
+//    string position;
+//
+//public:
+//    Employee(string n, int a, string p) : Person(n, a)
+//    {
+//        position = p;
+//    }
+//
+//    void displayEmployee()
+//    {
+//        displayPerson();
+//        cout << "Position: " << position << endl;
+//    }
+//};
+
+//int main()
+//{
+//    Employee e1("Alex", 30, "Developer");
+//    e1.displayEmployee();
+//
+//    return 0;
+//}
+class Shape {
+
+
+
+
+
+
+};
