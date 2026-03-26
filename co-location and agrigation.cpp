dfdf098d149f8d687788f0bddbebdeadf64f3f56
Lucyk6 <lucykazaryan16@gmail.com>
2026-03-26 11:08:55 +0300

    Create co-location and agrigation.cpp

---
+++ co-location and agrigation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+//class  Author {
+//public:
+//	void name() {
+//		cout << "smth" << endl;
+//	}
+//};
+//class Book {
+//private:
+//	Author* author;
+//
+//public:
+//	Book(Author* a) {
+//		author = a;
+//	 }
+//
+//	void run() {
+//		author->name();
+//	}
+//};
+//int main()
+//{
+//	Author a;
+//	Book b(&a);
+//	b.run();
+//}
+// 2)
+//class Furniture {
+//public:
+//	void chair() {
+//
+//		cout << "nice chair" << endl;
+//
+//	 }
+//};
+//class Room {
+//
+//private:
+//
+//	Furniture f;
+//public:
+//	void run() {
+//		f.chair();
+//	}
+//};
+//int main() {
+//	Room r;
+//	r.run();
+//}
